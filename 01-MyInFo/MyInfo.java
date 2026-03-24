6b721a42ed8ddde69e58ef58f63838c1109c6533
Pong <vx7vx8vx9@gmail.com>
2026-03-24 09:42:48 +0700

    Add MyInfo class and Java file with personal information display

---
+++ 01-MyInFo/MyInfo.java
@@ -0,0 +1,11 @@
+public class MyInfo {
+    public static void main(String[] args) {
+        String name = "Pong";
+        int age = 28;
+        String hobby = "coding";
+
+        System.out.println("My name is " + name);
+        System.out.println("I am " + age + " years old.");
+        System.out.println("My hobby is " + hobby + ".");
+    }
+}
